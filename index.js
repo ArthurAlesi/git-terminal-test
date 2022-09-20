6606900c2b5ce026b3dc19321b41601b9f0f43c8
Arthur Alesi <arthuralesi@gmail.com>
2022-09-20 17:45:11 -0300

    commit 5

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ console.log("commit 4");
 console.log("commit 4");
 console.log("commit 4");
 console.log("commit 4");
+
+
+console.log("commit 5");
+console.log("commit 5");
+console.log("commit 5");
+console.log("commit 5");
+console.log("commit 5");
